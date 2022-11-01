9fa619dd2c34e9ccf37f4b419a52667d72cb6a63
Fer <ferunonero@gmail.com>
2022-11-01 10:58:28 +0300

    Задача Первоначальный взнос по ипотеке

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,10 +7,10 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-        int costApartment = 500000;
+        final int costApartment = 500000;
         System.out.println("Стоимость квартиры : " + costApartment);
 
-        double firstPayMortgagePercent = 15;
+        final double firstPayMortgagePercent = 15;
         System.out.println("Процент первоначального взноса по ипотеке : " + firstPayMortgagePercent);
 
         double sumFirstPay = costApartment * (firstPayMortgagePercent/100);
